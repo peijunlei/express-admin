31f567e0839fd02d5f98ad6430e1484db4f8a77f
PJL <1594000695@qq.com>
2024-09-01 10:50:16 +0800

    add:配置文件提交

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: 'express-app',
-      script: 'server.js',
+      name: 'express-admin',
+      script: './lib/express-admin',
       env_production: {
-        NODE_ENV: "production"
-      },
-      env_development: {
-        NODE_ENV: "development",
+        NODE_ENV: "production",
+        PORT: 3000,
+        //  # jwt
+        JWT_SECRET: 'ABCDabcd1234567890',
+        // # jwt 过期时间
+        JWT_EXPIRES_IN: '1d',
+        // # cookie
+        JWT_COOKIE_EXPIRES_IN: 1,
+        // # mogodb_url
+        MONGODB_URL: 'mongodb://root:peijunlei@121.41.59.32:27017',
+        // #  数据库名
+        DB_NAME: 'blog',
+        QQ: 'junleipei@qq.com',
+        // # QQ_PASS
+        QQ_PASS: 'slqcyiywlhzkfeec',
       }
     }
   ]
